e72084878de4048a5604e3849cc1464ebad3664a
AlexHoecht <alex.hoecht@hotmail.com>
2018-03-21 11:03:20 -0400

    WIP!!!!!

--- src/main/java/com/sysc4806/project/models/Review.java
+++ src/main/java/com/sysc4806/project/models/Review.java
@@ -43,34 +43,66 @@ public class Review {
         this(null, 0);
     }
 
+    /**
+     * Getter method for the Review Id
+     * @return review id
+     */
     public Long getId() {
         return id;
     }
 
-    public UserEntity getAuthor() {
-        return author;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
+    /**
+     * Setter method for the Review id
+     * @param id new id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Getter method for the Review author
+     * @return review author
+     */
+    public UserEntity getAuthor() {
+        return author;
+    }
+
+    /**
+     * Setter method for the Review author
+     * @param author new author
+     */
     public void setAuthor(UserEntity author) {
         this.author = author;
     }
 
+    /**
+     * Getter method for the Review product
+     * @return review product
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * Setter method for the Review product
+     * @param product new product
+     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
+    /**
+     * Getter method for the Review rating
+     * @return review rating
+     */
+    public int getRating() {
+        return rating;
+    }
+
+    /**
+     * Setter method for the Review rating
+     * @param rating new rating
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
